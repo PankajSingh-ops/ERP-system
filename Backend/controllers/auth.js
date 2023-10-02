a0896b15e1b502906bd52ff729785fafbda7bebe
Pankaj <ps96716@gmail.com>
2023-10-02 23:42:52 +0530

    backend cgange

--- Backend/controllers/auth.js
+++ Backend/controllers/auth.js
@@ -1,17 +1,17 @@
-const User = require("../models/users_model");
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
 
-exports.postLogin = async (req, res, next) => {
-  // console.log(req.body);
-  const { Email, Password } = req.body;
-  // console.log(Email,Password);
+const User=require("../models/users_model")
+const jwt=require("jsonwebtoken")
+const bcrypt=require('bcrypt')
 
-  try {
-    const userData = await User.findOne({ Email: Email });
-    const hashPassword = await bcrypt.compare(Password, userData.Password);
+exports.postLogin=async(req,res,next)=>{
+    // console.log(req.body);
+    const{Email,Password}=req.body
+    // console.log(Email,Password);
+
+    try{
+       const userData=await User.findOne({Email:Email})
+       const hashPassword=await bcrypt.compare(Password,userData.Password)
     //    console.log(hashPassword);
-<<<<<<< HEAD
        if(hashPassword){
         const token=await jwt.sign({userId:userData._id,role:userData.Role},"It is a secret key",{expiresIn:"1h",})
         // console.log(token);
@@ -20,19 +20,5 @@ exports.postLogin = async (req, res, next) => {
     }catch(err){
         console.log(err.message);
         return res.status(500).json({message:err.message})
-=======
-    if (hashPassword) {
-      const token = jwt.sign(
-        { userId: userData._id, role: userData.Role },
-        "It is a secret key",
-        { expiresIn: "1h" }
-      );
-      // console.log(token);
-      res.status(200).json({ token: token, role: userData.Role });
->>>>>>> 385d4bf6b6fc659bfeffd781be7d86613b7a2dfe
     }
-  } catch (err) {
-    console.log(err.message);
-    return res.status(500).json({ message: err.message });
-  }
-};
+}
--- Backend/routes/admin.js
+++ Backend/routes/admin.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express=require("express")
 const {postAddUser, getUser, postDelete, postEdit, getEdit, managerData, employeeData, postDepartment, getDepartments, getCalender, postLeave}=require("../controllers/admin")
 const router=express.Router();
@@ -16,37 +15,3 @@ router.post("/calender", getCalender)
 router.post("/leave",postLeave)
 
 module.exports=router;
-=======
-const express = require("express");
-const {
-  postAddUser,
-  getUser,
-  postDelete,
-  postEdit,
-  getEdit,
-  managerData,
-  employeeData,
-  postDepartment,
-} = require("../controllers/admin");
-const router = express.Router();
-
-const authJWT = require("../middleware/jwt");
-
-router.post("/add-user", authJWT, postAddUser);
-
-router.get("/teams", getUser);
-
-router.get("/delete/:id", postDelete);
-
-router.get("/update/:id", getEdit);
-
-router.put("/update/:id", postEdit);
-
-router.get("/manager", managerData);
-
-router.get("/employee", employeeData);
-
-router.post("/department", postDepartment);
-
-module.exports = router;
->>>>>>> 385d4bf6b6fc659bfeffd781be7d86613b7a2dfe
